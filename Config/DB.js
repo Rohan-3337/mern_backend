bd61103c6ee466ed84909d8c35ed20ebe70ffd30
Rohan-3337 <durgawati7275@gmail.com>
2023-11-28 22:17:35 +0530

    datbase setting

--- Config/DB.js
+++ Config/DB.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const mongoURL = process.env.PORT || "mongodb+srv://Rohan:Roh%40n123@cluster0.cuw8aev.mongodb.net/"
 
 const ConnectDB = async() =>{
-    await mongoose.connect(mongoURL,{ useNewUrlParser: true,useNewUrlParser:true }).then(()=>{console.log("connected")}).catch((err)=>{console.log(err)})
+    await mongoose.connect(mongoURL,{   useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, }).then(()=>{console.log("connected")}).catch((err)=>{console.log(err)})
 }
 module.exports = ConnectDB;
